3c50c6fc48bbe935532c94e45daca3ba531f4c5f
HomePavluyk <testpavlyk@icloud.com>
2025-02-26 17:51:55 +0300

    Auto stash before merge of "develop" and "origin/develop"

--- develop.py
+++ develop.py
@@ -3,5 +3,18 @@ print("commit2")
 print("commit3")
 print("commit4")
 print("commit5")
+<<<<<<< HEAD
 a = int(input())
 b = RandomLine
+<<<<<<< Updated upstream:develop.py
+=======
+=======
+if 1+1:
+    print("Sobaka")
+elif:
+    print("cat")
+else:
+    print("fish")
+    print("NOOOOO")
+>>>>>>> main
+>>>>>>> Stashed changes:test.py
